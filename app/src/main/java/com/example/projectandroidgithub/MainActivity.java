f0e8e6a9417d1cde19c120bef0d1cfecfd3d4ccd
badrussamsi <badrus.samsi22@gmail.com>
2019-12-07 10:23:58 +0700

    Add progress bar in MainActivity

--- app/src/main/java/com/example/projectandroidgithub/MainActivity.java
+++ app/src/main/java/com/example/projectandroidgithub/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.projectandroidgithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.pb_test);
     }
 }
